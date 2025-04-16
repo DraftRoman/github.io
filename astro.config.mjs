41a4b5ac202d72315aefe52102ceaff1e3dc8d8a
Roman <151454298+DraftRoman@users.noreply.github.com>
2025-04-16 16:05:00 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import siteConfig from './src/data/site-config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.io',
+    site: 'https://draftroman.github.io',
     vite: {
         plugins: [tailwindcss()]
     },
